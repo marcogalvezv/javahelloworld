4ac8b01b3288fe2fc657261cddf58dca2dc61af5
mgalvez <mgalvez@frontsteps.com>
2021-04-30 17:29:20 -0400

    third commit

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello-world")
 public class HelloWorldController {
-
+    @CrossOrigin
     @GetMapping
     public String HelloWorld()
     {
