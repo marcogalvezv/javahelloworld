fcf6e6ebbe9a84d9a19b1cd842de6c3b5db0c0a1
mgalvez <mgalvez@frontsteps.com>
2021-04-30 17:40:23 -0400

    fifth commit

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -5,13 +5,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/hello-world")
 public class HelloWorldController {
     @CrossOrigin("*")
     @GetMapping
-    public String HelloWorld()
+    public List<Person> HelloWorld()
     {
-        return "HelloWorld";
+        List<Person> people = new ArrayList<>();
+        Person p = new Person();
+        p.id="1";
+        p.name="MarcoGalvez";
+        people.add(p);
+        return people;
+    }
+
+    public class Person
+    {
+      public String id;
+      public String name;
     }
 }
+
